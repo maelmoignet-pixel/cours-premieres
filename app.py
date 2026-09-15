cc25a11303e3442c50477c6cc629f0936ce96126
maelmoignet-pixel <mael.moignet@gmail.com>
2026-09-15 19:25:41 +0200

    Uncomment DS 1 entry in app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ FICHIERS = {
              {"nom": "Évaluation 3", "fichier": "second-degre-2025-eval3.html"},
         ],
         "ds_2025": [
-            # {"nom": "DS 1", "fichier": "second-degre-2025-DS1.pdf"},
+             {"nom": "DS 1", "fichier": "second-degre-2025-DS1.pdf"},
         ],
         "eval_2026": [],
         "ds_2026": [],
